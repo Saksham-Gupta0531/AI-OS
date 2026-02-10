76fccc23e09b9c077ce478ba8049e8be7004eef6
sidra <sidrajagrawal@gmail.com>
2026-02-10 23:42:28 +0530

    Mascot Try

--- web_marketplace/frontend/src/Home/Home.jsx
+++ web_marketplace/frontend/src/Home/Home.jsx
@@ -1,26 +1,27 @@
 import bgVideo from '../media/credia2.mp4';
 import phoneBg from '../media/phone_poster.png';
+import NewBg from '../media/NewBg.webp';
 import AgentSpline from '../Agent3D/AgentSpline';
 
 function Home() {
   return (
-    <div className="relative grid h-screen w-full overflow-x-hidden">
+    <div className="h-screen w-screen overflow-hidden">
       <img
-        src={phoneBg}
-        alt="Phone Background"
-        className="col-start-1 row-start-1 h-full w-full object-cover md:hidden"
+        src={NewBg}
+        alt="Background"
+        className="w-full h-full object-contain"
       />
 
-      <video
+      {/* <video
         className="col-start-1 row-start-1 h-full w-full object-cover hidden md:block"
         src={bgVideo}
         autoPlay
         loop
         muted
         playsInline
-      />
+      /> */}
 
-      <AgentSpline />
+      {/* <AgentSpline /> */}
     </div>
   );
 }
