66d69ed6797639ac80f100aac2539e0e63609122
Rachit Upadhyay <urachit08@gmail.com>
2026-04-30 23:19:38 +0530

    Updated Index.js

--- web_marketplace/backend/routes/index.js
+++ web_marketplace/backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const route = express.Router();
 const authRoute = require('./auth');
+const packageRoute = require('./packageRoutes');
 const { authLimiter, apiLimiter } = require('../middleware/rateLimiter');
 
 route.use('/auth', authLimiter, authRoute);
+route.use('/packages', packageRoute);
 
 module.exports = route;
